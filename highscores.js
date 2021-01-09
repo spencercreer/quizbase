e691c3fa52bf565fe87f9890e5079870bd523023
spencercreer <spencercreer@gmail.com>
2021-01-08 17:39:23 -0700

    stored initals and score to local storage

--- highscores.js
+++ highscores.js
@@ -1,11 +1,28 @@
-var submitButton = document.getElementById("button-addon1");
-var initialsPage = document.getElementById("initials");
-var highscoresPage = document.getElementById("highscores");
+var submitButton = document.getElementById("submitBtn");
+var initialsPage = document.getElementById("initialsPg");
+var highscoresPage = document.getElementById("highscoresPg");
 
+var scoreEl = document.querySelector(".score");
+var totScore = 0;
+
+// On Submit button click, hide initialsPage and show highscoresPage
 function initialSubmit(){
-    console.log("initials submitted")
+    
     initialsPage.hidden = true;
     highscoresPage.hidden = false;
+
+    // Append initials and score to highscores list
+    var plInit = document.getElementById("initials").value;
+    var playerScore = plInit + ":     " + totScore;
+    localStorage.setItem("playerScore", playerScore)
+    
+    var node = document.createElement("LI");  
+    var textnode = document.createTextNode(playerScore);              
+    node.appendChild(textnode);                             
+    document.getElementById("scoresList").appendChild(node);
+ 
+   
 }
+
 
  submitButton.onclick = initialSubmit;
