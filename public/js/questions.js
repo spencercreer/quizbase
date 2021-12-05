767c57949c8b25e37f848cc9ae5a3c673639206c
spencercreer <spencercreer@gmail.com>
2021-12-05 01:15:59 -0700

    populate add and edit question modal

--- public/js/questions.js
+++ public/js/questions.js
@@ -1,11 +1,22 @@
 const addModalBtn = document.getElementById('add-modal-btn').addEventListener('click', function() {
     document.getElementById('question-modal-title').textContent = 'Add Question'
+    document.getElementById('question').value = ''
+    document.getElementById('answer').value = ''
+    document.getElementById('choice_a').value = ''
+    document.getElementById('choice_b').value = ''
+    document.getElementById('choice_c').value = ''
 })
 
 const editBtns = document.querySelectorAll('.edit-button')
 for (const btn of editBtns) {
     btn.addEventListener('click', function (event) {
         document.getElementById('question-modal-title').textContent = 'Edit Question'
+        document.getElementById('question').value = this.nextElementSibling.textContent
+        document.getElementById('answer').value = this.parentNode.nextElementSibling.children[0].textContent
+        document.getElementById('choice_a').value = this.parentNode.nextElementSibling.children[1].textContent
+        document.getElementById('choice_b').value = this.parentNode.nextElementSibling.children[2].textContent
+        document.getElementById('choice_c').value = this.parentNode.nextElementSibling.children[3].textContent
+        
     })
 }
 
