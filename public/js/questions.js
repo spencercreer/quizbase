92e63bf80bc250ec9d3ae037f7f8592c18b2a16b
spencercreer <spencercreer@gmail.com>
2021-12-05 00:36:31 -0700

    added question modal and btn styling

--- public/js/questions.js
+++ public/js/questions.js
@@ -1,3 +1,14 @@
+const addModalBtn = document.getElementById('add-modal-btn').addEventListener('click', function() {
+    document.getElementById('question-modal-title').textContent = 'Add Question'
+})
+
+const editBtns = document.querySelectorAll('.edit-button')
+for (const btn of editBtns) {
+    btn.addEventListener('click', function (event) {
+        document.getElementById('question-modal-title').textContent = 'Edit Question'
+    })
+}
+
 const deleteBtns = document.querySelectorAll(".delete-button")
 for (const btn of deleteBtns) {
     btn.addEventListener('click', function (event) {
@@ -15,14 +26,14 @@ for (const btn of deleteBtns) {
     })
 }
 
-const editBtns = document.querySelectorAll(".edit-button")
-for (const btn of editBtns) {
-    btn.addEventListener('click', function (event) {
-        const id = this.getAttribute('data-id')
-        console.log(id + ' edit button clicked')
-        // fetch(`api/delete/${id}`, {
-        //     method: 'DELETE',
-        // })
-        //     .then(window.location.reload())
-    })
-}
+// const editBtns = document.querySelectorAll(".edit-button")
+// for (const btn of editBtns) {
+//     btn.addEventListener('click', function (event) {
+//         const id = this.getAttribute('data-id')
+//         console.log(id + ' edit button clicked')
+//         // fetch(`api/delete/${id}`, {
+//         //     method: 'DELETE',
+//         // })
+//         //     .then(window.location.reload())
+//     })
+// }
