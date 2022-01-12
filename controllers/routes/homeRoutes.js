74c86463a42b820d701e47496ba11b83182a1c90
spencercreer <spencercreer@gmail.com>
2022-01-11 22:49:24 -0700

    logout home route

--- controllers/routes/homeRoutes.js
+++ controllers/routes/homeRoutes.js
@@ -11,6 +11,10 @@ router.get('/login', (req, res) => {
     res.render('login')
 })
 
+router.get('/logout', (req, res) => {
+    res.render('login')
+})
+
 router.get('/signup', (req, res) => {
     res.render('signup')
 })
